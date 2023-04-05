333ad36c432d0e1c4029b51d5dc22b4cfc496106
Noah Cowan <nsncowan@gmail.com>
2023-04-04 19:28:49 -0700

    add switch login/logout in navbar
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Mike Wilkes <mikewilkes@outlook.com>

--- Library/ViewModels/LoginViewModel.cs
+++ Library/ViewModels/LoginViewModel.cs
@@ -5,9 +5,7 @@ namespace Library.ViewModels
   public class LoginViewModel
   {
     [Required]
-    [EmailAddress]
-    [Display(Name = "Email Address")]
-    public string Email { get; set; }
+    public string UserName { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
